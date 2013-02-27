b78a67d689a47c8611558d45cad7252339618914
Leonid Grinberg <leonidg@mit.edu>
2013-02-27 14:32:02 -0500

    Starting code.

---
+++ src/differentiator/Evaluator.java
@@ -0,0 +1,11 @@
+package differentiator;
+
+/**
+ * An interface requiring the evaluate(String,String) method.
+ * Your Differentiator <b>must</b> implement this interface.
+ *
+ * <b>DO NOT MODIFY THIS INTERFACE!</b>
+ */
+public interface Evaluator {
+    public String evaluate(String expression, String variable);
+}
