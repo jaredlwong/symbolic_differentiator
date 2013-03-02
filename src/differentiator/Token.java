b4233d939d90af4f56e2b9ae94da00b50fccdc67
Jared Wong <jaredlwong@gmail.com>
2013-03-02 13:20:11 -0500

    finished token class

--- src/differentiator/Token.java
+++ src/differentiator/Token.java
@@ -1,5 +1,10 @@
 package differentiator;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.EnumMap;
+import java.util.Map;
+
 /**
  * A token is a lexical item that the parser uses.
  */
@@ -8,8 +13,195 @@ public class Token {
      * All the types of tokens that can be made.
      */
     public static enum Type {
-        // TODO write me.
+        // operators
+        LPAR("("), RPAR(")"), PLUS("+"),
+        MINUS("-"), STAR("*"), SLASH("/"),
+        CARET("^"),
+
+        // non-operators
+        INTEGER("Z"),
+        REAL("R"),
+        IDENTIFIER("X");
+
+        /**
+         * rep is the String representation of the Type.
+         * Stored as String in anticipation of multi-character type
+         * representations.
+         */
+        private final String rep;
+
+        private Type(String rep) {
+            this.rep = rep;
+        }
+
+        @Override
+        public String toString() {
+            return rep;
+        }
     }
 
-    // TODO write me
+    /** 
+     * The map of operator Types to Tokens. Note that all operator Tokens are
+     * singletons.
+     */
+    private static final Map<Type, Token> OPMAP =
+            new EnumMap<Type, Token>(Type.class);
+    static {
+        for (Type type : Type.values()) {
+            OPMAP.put(type, new Token(type));
+        }
+        OPMAP.remove(Type.INTEGER);
+        OPMAP.remove(Type.REAL);
+        OPMAP.remove(Type.IDENTIFIER);
+    }
+
+    /** The type of the Token */
+    private final Type type;
+
+    /** The value of the Token if not operator */
+    private Object value;
+    private Class<?> valueClass;
+
+    /**
+     * Private constructor for operators. Enforces singleton property.
+     * @param type The Type of the Token
+     */
+    private Token(Type type) {
+        this.type = type;
+    }
+
+    /**
+     * Private constructor for non-operator Tokens. Accepts the String
+     * value of the Token.
+     * @param type The Type of the Token
+     * @param value The String value of the Token
+     */
+    private Token(Type type, String value) {
+        this(type);
+        switch (type) {
+            case INTEGER:
+                this.value = new BigInteger(value);
+                valueClass = BigInteger.class;
+                break;
+            case REAL:
+                this.value = new BigDecimal(value);
+                valueClass = BigDecimal.class;
+                break;
+            case IDENTIFIER:
+                this.value = value;
+                valueClass = String.class;
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Get instance of Token class, if valid token.
+     * @param token The token that needs to be represented
+     * @return An instance of Token that represents token
+     */
+    public static Token getInstance(String token) {
+        Type type = getType(token);
+        if (OPMAP.containsKey(type)) {
+            return OPMAP.get(type);
+        }
+        return new Token(type, token);
+    }
+
+    /**
+     * Get Type of a token
+     * @param token The token that is being parsed
+     * @return The Type of the token, an InvalidTokenException is thrown if
+     *          token not recognized.
+     */
+    private static Type getType(String token) {
+        for (Type type : OPMAP.keySet()) {
+            if (token.equals(type.rep)) return type;
+        }
+        if (token.matches("-?[0-9]+")) return Type.INTEGER;
+        if (token.matches("-?[0-9]*\\.[0-9]+|-?[0-9]+\\.[0-9]*")) return Type.REAL;
+        if (token.matches("[a-zA-Z]+")) return Type.IDENTIFIER;
+        throw new InvalidTokenException("Invalid Token: " + token);
+    }
+
+    /** 
+     * @return The Type of the Token
+     */
+    public Type getType() {
+        return type;
+    }
+
+    public boolean isOperator() {
+        return OPMAP.containsKey(type);
+    }
+
+    /**
+     * Can only call getValue on non-operator Tokens.
+     * @return value of non-operator Token
+     */
+    public Object getValue() {
+        return value;
+    }
+
+    public Class<?> getValueClass() {
+        return valueClass;
+    }
+
+    @Override
+    public String toString() {
+        if (OPMAP.containsKey(type)) {
+            return OPMAP.get(type).toString();
+        }
+        return this.value.toString();
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        result = prime * result
+                + ((valueClass == null) ? 0 : valueClass.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Token)) {
+            return false;
+        }
+        Token other = (Token) obj;
+        if (type != other.type) {
+            return false;
+        }
+        if (value == null) {
+            if (other.value != null) {
+                return false;
+            }
+        } else if (!value.equals(other.value)) {
+            return false;
+        }
+        if (valueClass == null) {
+            if (other.valueClass != null) {
+                return false;
+            }
+        } else if (!valueClass.equals(other.valueClass)) {
+            return false;
+        }
+        return true;
+    }
 }
---
+++ src/differentiator/TokenTest.java
@@ -0,0 +1,104 @@
+package differentiator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TokenTest {
+    @Test
+    public void basicOperatorsTests() {
+        Token lpar = Token.getInstance("(");
+        assertEquals(Token.Type.LPAR, lpar.getType());
+
+        Token rpar = Token.getInstance(")");
+        assertEquals(Token.Type.RPAR, rpar.getType());
+
+        Token star = Token.getInstance("*");
+        assertEquals(Token.Type.STAR, star.getType());
+    }
+
+    @Test
+    public void basicIdentifierTests() {
+        Token lower1 = Token.getInstance("a");
+        assertEquals(Token.Type.IDENTIFIER, lower1.getType());
+
+        Token lower2 = Token.getInstance("z");
+        assertEquals(Token.Type.IDENTIFIER, lower2.getType());
+
+        Token upper1 = Token.getInstance("A");
+        assertEquals(Token.Type.IDENTIFIER, upper1.getType());
+
+        Token upper2 = Token.getInstance("Z");
+        assertEquals(Token.Type.IDENTIFIER, upper2.getType());
+
+        Token lowermulti = Token.getInstance("aazz");
+        assertEquals(Token.Type.IDENTIFIER, lowermulti.getType());
+
+        Token uppermulti = Token.getInstance("AAZZ");
+        assertEquals(Token.Type.IDENTIFIER, uppermulti.getType());
+
+        char fullAlpha[] = new char[26*2];
+        int i = 0;
+        for (char c = 'a'; c <= 'z'; ++c, ++i) {
+            fullAlpha[i*2] = c;
+            fullAlpha[i*2+1] = Character.toUpperCase(c);
+        }
+        Token mixedcase = Token.getInstance(new String(fullAlpha));
+        assertEquals(Token.Type.IDENTIFIER, mixedcase.getType());
+    }
+
+    @Test
+    public void basicIntegerTests() {
+        Token simple = Token.getInstance("0123456789");
+        assertEquals(Token.Type.INTEGER, simple.getType());
+
+        Token negsimple = Token.getInstance("-0123456789");
+        assertEquals(Token.Type.INTEGER, negsimple.getType());
+
+        Token duplicate = Token.getInstance("-00112233445566778899");
+        assertEquals(Token.Type.INTEGER, duplicate.getType());
+    }
+
+    @Test
+    public void basicRealTests() {
+        Token simple1 = Token.getInstance("0123456789.");
+        assertEquals(Token.Type.REAL, simple1.getType());
+
+        Token simple2 = Token.getInstance(".0123456789");
+        assertEquals(Token.Type.REAL, simple2.getType());
+
+        Token simple3 = Token.getInstance("01234.56789");
+        assertEquals(Token.Type.REAL, simple3.getType());
+
+        Token negsimple1 = Token.getInstance("-.0123456789");
+        assertEquals(Token.Type.REAL, negsimple1.getType());
+
+        Token negsimple2 = Token.getInstance("-0123456789.");
+        assertEquals(Token.Type.REAL, negsimple2.getType());
+
+        Token negsimple3 = Token.getInstance("-012345.6789");
+        assertEquals(Token.Type.REAL, negsimple3.getType());
+    }
+
+    @Test
+    public void equalityTests() {
+        Token int1 = Token.getInstance("1000000000000000000000000000000000");
+        Token int2 = Token.getInstance("1000000000000000000000000000000000");
+        assertEquals(int1,int2);
+        assertEquals(int1.hashCode(), int2.hashCode());
+        assertTrue(int1.equals(int2));
+
+        Token dec1 = Token.getInstance("123.123123123123000000000000000123");
+        Token dec2 = Token.getInstance("123.123123123123000000000000000123");
+        assertEquals(dec1,dec2);
+        assertEquals(dec1.hashCode(), dec2.hashCode());
+        assertTrue(dec1.equals(dec2));
+        
+        Token var1 = Token.getInstance("foobar");
+        Token var2 = Token.getInstance("foobar");
+        assertEquals(var1, var2);
+        assertEquals(var1.hashCode(),var2.hashCode());
+        assertTrue(var1.equals(var2));
+    }
+}
