f906ca7fb01692f55448c42cc4819b3af13a6376
Jared Wong <jaredlwong@gmail.com>
2013-03-06 15:41:41 -0500

    unfinisehd

--- src/differentiator/PolynomialTerm.java
+++ src/differentiator/PolynomialTerm.java
@@ -1,6 +1,7 @@
 package differentiator;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,15 +15,25 @@ public class PolynomialTerm {
         variables = new LinkedList<String>(_variables);
     }
 
-    public void multiply(String value) {
-        if (value.matches("-?[0-9]+") ||
-                value.matches("-?[0-9]*\\.[0-9]+|-?[0-9]+\\.[0-9]*")) {
-            BigDecimal mult = new BigDecimal(value);
-            coefficient = coefficient.multiply(mult);
-        } else if (value.matches("[a-zA-Z]+")) {
-            variables.add(value);
-        } else {
-            throw new IllegalArgumentException("Invalid multiplicand");
-        }
+    public PolynomialTerm(BigDecimal _coefficient, List<String> _variables) {
+        coefficient = _coefficient;
+        // create shallow copy, okay because Strings 
+        variables = new LinkedList<String>(_variables);
+    }
+
+    public static PolynomialTerm multiply(
+            PolynomialTerm one, PolynomialTerm two) {
+        List<String> newVars = new ArrayList<String>(one.variables);
+        newVars.addAll(two.variables);
+        BigDecimal newCoeff = one.coefficient.add(two.coefficient);
+        return new PolynomialTerm(newCoeff,newVars);
+    }
+
+    public String[] getVariables() {
+        return variables.toArray(new String[variables.size()]);
+    }
+
+    public BigDecimal getCoefficient() {
+        return coefficient;
     }
 }
---
+++ src/differentiator/PolynomialTermTest.java
@@ -0,0 +1,11 @@
+package differentiator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PolynomialTermTest {
+    List<String> vars = new ArrayList<String>(1);
+    vars.
+    PolynomialTerm a = new PolynomialTerm(10,vars);
+    String[] expectedVars = 
+}
