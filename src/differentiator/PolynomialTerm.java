26bd5e935672aa10646d6f332a74e2f98bd46bc2
Jared Wong <jaredlwong@gmail.com>
2013-03-06 12:02:51 -0500

    poly stuff

---
+++ src/differentiator/PolynomialTerm.java
@@ -0,0 +1,28 @@
+package differentiator;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PolynomialTerm {
+    private BigDecimal coefficient;
+    private List<String> variables;
+
+    public PolynomialTerm(int _coefficient, List<String> _variables) {
+        coefficient = BigDecimal.valueOf(_coefficient);
+        // create shallow copy, okay because Strings 
+        variables = new LinkedList<String>(_variables);
+    }
+
+    public void multiply(String value) {
+        if (value.matches("-?[0-9]+") ||
+                value.matches("-?[0-9]*\\.[0-9]+|-?[0-9]+\\.[0-9]*")) {
+            BigDecimal mult = new BigDecimal(value);
+            coefficient = coefficient.multiply(mult);
+        } else if (value.matches("[a-zA-Z]+")) {
+            variables.add(value);
+        } else {
+            throw new IllegalArgumentException("Invalid multiplicand");
+        }
+    }
+}
