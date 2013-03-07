7c14fb7325d909b38d36ee8c8a906677a0edd668
Jared Wong <jaredlwong@gmail.com>
2013-03-07 03:05:59 -0500

    comments

--- src/differentiator/ast/PrintTreeVisitor.java
+++ src/differentiator/ast/PrintTreeVisitor.java
@@ -1,19 +1,33 @@
 package differentiator.ast;
 
-
+/**
+ * The Class PrintTreeVisitor defines an example ExpressionEvaluationVisitor
+ * that prints out an ExpressionElement tree with indentation.
+ */
 public class PrintTreeVisitor implements ExpressionEvaluationVisitor {
+    
+    /** The indent. */
     private StringBuilder indent = new StringBuilder("");
 
+    /* (non-Javadoc)
+     * @see differentiator.ast.ExpressionEvaluationVisitor#visit(differentiator.ast.NumberExpression)
+     */
     @Override
     public void visit(NumberExpression expression) {
         System.out.println(indent.toString() + expression);
     }
 
+    /* (non-Javadoc)
+     * @see differentiator.ast.ExpressionEvaluationVisitor#visit(differentiator.ast.IdentifierExpression)
+     */
     @Override
     public void visit(IdentifierExpression expression) {
         System.out.println(indent.toString() + expression);
     }
 
+    /* (non-Javadoc)
+     * @see differentiator.ast.ExpressionEvaluationVisitor#visit(differentiator.ast.SumExpression)
+     */
     @Override
     public void visit(SumExpression expression) {
         System.out.println(indent.toString() + expression);
@@ -24,6 +38,9 @@ public class PrintTreeVisitor implements ExpressionEvaluationVisitor {
         indent.deleteCharAt(indent.length()-1);
     }
 
+    /* (non-Javadoc)
+     * @see differentiator.ast.ExpressionEvaluationVisitor#visit(differentiator.ast.ProductExpression)
+     */
     @Override
     public void visit(ProductExpression expression) {
         System.out.println(indent.toString() + expression);
@@ -34,6 +51,9 @@ public class PrintTreeVisitor implements ExpressionEvaluationVisitor {
         indent.deleteCharAt(indent.length()-1);
     }
 
+    /* (non-Javadoc)
+     * @see differentiator.ast.ExpressionEvaluationVisitor#visit(differentiator.ast.DifferenceExpression)
+     */
     @Override
     public void visit(DifferenceExpression expression) {
         System.out.println(indent.toString() + expression);
@@ -44,6 +64,9 @@ public class PrintTreeVisitor implements ExpressionEvaluationVisitor {
         indent.deleteCharAt(indent.length()-1);
     }
 
+    /* (non-Javadoc)
+     * @see differentiator.ast.ExpressionEvaluationVisitor#visit(differentiator.ast.QuotientExpression)
+     */
     @Override
     public void visit(QuotientExpression expression) {
         System.out.println(indent.toString() + expression);
@@ -54,6 +77,9 @@ public class PrintTreeVisitor implements ExpressionEvaluationVisitor {
         indent.deleteCharAt(indent.length()-1);
     }
 
+    /* (non-Javadoc)
+     * @see differentiator.ast.ExpressionEvaluationVisitor#visit(differentiator.ast.ExponentialExpression)
+     */
     @Override
     public void visit(ExponentialExpression expression) {
         System.out.println(indent.toString() + expression);
