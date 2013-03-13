cd17a57d8e22c36958f78d7ab33a51f0ed0e14f0
Jared L Wong <jaredw@W20-575-59.MIT.EDU>
2013-03-13 15:20:11 -0400

    added bad tests to parser

--- src/differentiator/error/InvalidTokenException.java
+++ src/differentiator/error/InvalidTokenException.java
@@ -6,9 +6,9 @@ public class InvalidTokenException extends IllegalArgumentException {
      */
     private static final long serialVersionUID = 1L;
 
-    public InvalidTokenException() {}
+    private InvalidTokenException() {}
 
-    public InvalidTokenException(String message)
+    private InvalidTokenException(String message)
     {
        super(message);
     }
